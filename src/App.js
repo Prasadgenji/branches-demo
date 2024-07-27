8077c9b566a65f1079d1bd58bdf3b13f4dc458bf
prasadgenji <prasadgenji15@gmail.com>
2024-07-27 13:05:00 +0530

    commit 1 -- structure of app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import Attendance from "./Attendance";
-import Classes from "./Classes";
+
+
 
 function App() {
   return (
     <>
       <h1>
         KodNest App
-        
-        <Teachers />
-        <Classes />
-        <Attendance />
       </h1>
     </>
   );
